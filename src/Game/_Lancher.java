c42c7cc1756f9271cc734dfca8dc74989aa0a7d6
Raúl Del Olmo Alcocer <rauldelolmo2003@protonmail.com>
2023-05-04 11:36:26 +0200

    Añadiendo proyecto

---
+++ src/Game/_Lancher.java
@@ -0,0 +1,10 @@
+package Game;
+
+public class _Lancher {
+
+	public static void main(String[] args) {
+                System.out.println("Hola Mundo soy raul y voy a testear este juego");
+		new WindowLanch();
+                
+	}
+}
